e6d683f1a39c376222304a946d5e50d5893c6214
Arahnell <evrybreath_math@hotmail.fr>
2019-11-21 12:12:14 +0100

    Add files via upload
    Debut Projet

---
+++ ProjetObjetEsport/AjoutEquipes.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetObjetEsport
+{
+    /// <summary>
+    /// Logique d'interaction pour AjoutEquipes.xaml
+    /// </summary>
+    public partial class AjoutEquipes : Window
+    {
+        public AjoutEquipes()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            MainWindow sW = new MainWindow();
+            sW.Show();
+            this.Close();
+        }
+    }
+}
---
+++ ProjetObjetEsport/CreationTournoi.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetObjetEsport
+{
+    /// <summary>
+    /// Logique d'interaction pour CreationTournoi.xaml
+    /// </summary>
+    public partial class CreationTournoi : Window
+    {
+        public CreationTournoi()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            AjoutEquipes sW = new AjoutEquipes();
+            sW.Show();
+            this.Close();
+        }
+    }
+}
